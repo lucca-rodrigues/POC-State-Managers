9c658153840633984b6ec964573e81019f24ba06
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2024-07-23 20:30:55 -0300

    feat: create initial structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const entryPoint = path.resolve(__dirname, `${mode}/src/main.jsx`);
+
+  return {
+    plugins: [react()],
+    root: path.resolve(__dirname, mode),
+    build: {
+      outDir: path.resolve(__dirname, "dist", mode),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, mode, "src"),
+      },
+    },
+    server: {
+      open: true,
+    },
+  };
+});
