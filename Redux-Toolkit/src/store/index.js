f31d8e8e6ec366fd4d477e67126ce0e2595a376e
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2024-07-23 21:56:11 -0300

    feat: configure redux toolkit initial sample

---
+++ Redux-Toolkit/src/store/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import addressReducer from "../address.store";
+import counterReducer from "../counter.store";
+
+export default configureStore({
+  reducer: {
+    address: addressReducer,
+    counter: counterReducer,
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
-export default defineConfig(({ mode }) => {
-  const entryPoint = path.resolve(__dirname, `${mode}/src/main.jsx`);
-
-  return {
-    plugins: [react()],
-    root: path.resolve(__dirname, mode),
-    build: {
-      outDir: path.resolve(__dirname, "dist", mode),
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, mode, "src"),
-      },
-    },
-    server: {
-      open: true,
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
 });
